f18a578f6768facd89191a5b3a628bda18908fca
Joao Pedro <jpschimidtfuentes@gmail.com>
2025-09-29 16:54:50 -0300

    salvando para começar a aula exibindo menu

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -1,11 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	var nome string = "Biscatinha barata "
-	var idade int = 69
-	var versao float32 = 2.4
+	nome := "Biscatinha barata"
+	idade := 69
+	versao := 2.4
+
 	fmt.Println("Olá, Sr.", nome, "sua idade é", idade)
-	fmt.Println("Este programa esta na versão", versao)
+	fmt.Println("Este programa está na versão", versao)
 }
