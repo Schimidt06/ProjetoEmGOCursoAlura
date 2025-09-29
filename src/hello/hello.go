c346bba12b92f22198969e8bcd03a560b4af1840
Joao Pedro <jpschimidtfuentes@gmail.com>
2025-09-29 16:38:49 -0300

    atualizando para dar o cu

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -5,7 +5,7 @@ import "fmt"
 func main() {
 	var nome string = "Biscatinha barata "
 	var idade int = 69
-	var versao float32 = 1.1
+	var versao float32 = 2.4
 	fmt.Println("Olá, Sr.", nome, "sua idade é", idade)
 	fmt.Println("Este programa esta na versão", versao)
 }
