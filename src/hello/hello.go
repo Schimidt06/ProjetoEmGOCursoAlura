8a088b7aced1a13dae7242bb6c647856ab2671d1
Joao Pedro <jpschimidtfuentes@gmail.com>
2025-09-29 16:23:50 -0300

    atualizando para teste novamente

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -3,9 +3,9 @@ package main
 import "fmt"
 
 func main() {
-    var nome string = "Penis Murcho"
-    var idade int = 0
-    var versao float32 = 1.1
-    fmt.Println("Olá, Sr.", nome, "sua idade é", idade)
-    fmt.Println("Este programa esta na versão .", versao)
+	var nome string = "Biscatinha barata "
+	var idade int = 69
+	var versao float32 = 1.1
+	fmt.Println("Olá, Sr.", nome, "sua idade é", idade)
+	fmt.Println("Este programa esta na versão", versao)
 }
