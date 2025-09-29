a6f705516bb90ed5074d007c886ae4e2093224df
Joao Pedro <jpschimidtfuentes@gmail.com>
2025-09-29 18:03:15 -0300

    Finalizado o ec=xibindo menu pra ir embora

--- src/hello/hello.go
+++ src/hello/hello.go
@@ -5,8 +5,17 @@ import (
 )
 
 func main() {
-	nome := "Biscatinha barata"
+	nome := "João Schimidt"
 	versao := 2.4
 	fmt.Println("Olá, Sr.", nome)
 	fmt.Println("Este programa está na versão", versao)
+
+	fmt.Println("1-Inicar Monitoramento")
+	fmt.Println("2-Exibir Logs")
+	fmt.Println("0-Fechar essa bosta")
+
+	var comando int
+	fmt.Scan(&comando)
+	fmt.Println(" O Endereço de memória da variavel comando é", &comando)
+	fmt.Println("A variavel escolhida foi", comando)
 }
