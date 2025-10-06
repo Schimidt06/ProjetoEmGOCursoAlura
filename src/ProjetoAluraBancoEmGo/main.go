431b7cd26ef6ab76021e4aabc64910108e9853f0
Joao Pedro <jpschimidtfuentes@gmail.com>
2025-10-06 15:52:22 -0300

    finalizei tudo

--- src/ProjetoAluraBancoEmGo/main.go
+++ src/ProjetoAluraBancoEmGo/main.go
@@ -5,10 +5,26 @@ import (
 	"fmt"
 )
 
+// A INTERFACE FOI CORRIGIDA AQUI: agora espera o retorno de (string, float64)
+type verificarConta interface {
+	Sacar(valor float64) (string, float64)
+}
+
+// A FUNÇÃO FOI CORRIGIDA AQUI: agora ela lida com os retornos do Sacar
+func PagarBoleto(conta verificarConta, valorDoBoleto float64) {
+	status, _ := conta.Sacar(valorDoBoleto) // Usamos o blank identifier _ para ignorar o saldo retornado
+	fmt.Println("Status do Pagamento do Boleto:", status)
+}
+
 func main() {
 	contaDoDenis := contas.ContaPoupanca{}
 	contaDoDenis.Depositar(100)
-	contaDoDenis.Sacar(5000)
-	fmt.Println(contaDoDenis.ObterSaldo())
+	PagarBoleto(&contaDoDenis, 60)
 
+	fmt.Println("Saldo final:", contaDoDenis.ObterSaldo())
+
+	contaDaLaura := contas.ContaCorrente{}
+	contaDaLaura.Depositar(200)
+	PagarBoleto(&contaDaLaura, 80)
+	fmt.Println("Saldo final:", contaDaLaura.ObterSaldo())
 }
