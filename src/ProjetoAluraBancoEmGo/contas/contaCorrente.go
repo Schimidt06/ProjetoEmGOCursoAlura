71cadc5c6608f52c3f6e2023d1a4cf7044dc7816
Joao Pedro <jpschimidtfuentes@gmail.com>
2025-10-06 15:01:23 -0300

    finalinei o caitulo 4 da buceitnha.com

--- src/ProjetoAluraBancoEmGo/contas/contaCorrente.go
+++ src/ProjetoAluraBancoEmGo/contas/contaCorrente.go
@@ -1,37 +1,43 @@
 package contas
 
+import "ProjetoAluraBancoEmGo/clientes"
+
 type ContaCorrente struct {
-	Titular       string
+	Titular       clientes.Titular
 	NumeroAgencia int
 	NumeroConta   int
-	Saldo         float64
+	saldo         float64
 }
 
 func (c *ContaCorrente) Sacar(valorDoSaque float64) (string, float64) {
-	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.Saldo
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
 	if podeSacar {
-		c.Saldo -= valorDoSaque
-		return "Saque realizado com sucesso", c.Saldo
+		c.saldo -= valorDoSaque
+		return "Saque realizado com sucesso", c.saldo
 	} else {
-		return "Saldo insuficiente", c.Saldo
+		return "saldo insuficiente", c.saldo
 	}
 }
 
 func (c *ContaCorrente) Depositar(valorDoDeposito float64) (string, float64) {
 	if valorDoDeposito > 0 {
-		c.Saldo += valorDoDeposito
-		return "Depósito realizado com sucesso", c.Saldo
+		c.saldo += valorDoDeposito
+		return "Depósito realizado com sucesso", c.saldo
 	} else {
-		return "Valor inválido para depósito", c.Saldo
+		return "Valor inválido para depósito", c.saldo
 	}
 }
 
 func (c *ContaCorrente) Transferir(valorDaTransferencia float64, contadestino *ContaCorrente) bool {
-	if valorDaTransferencia < c.Saldo && valorDaTransferencia > 0 {
-		c.Saldo -= valorDaTransferencia
+	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
+		c.saldo -= valorDaTransferencia
 		contadestino.Depositar(valorDaTransferencia)
 		return true
 	} else {
 		return false
 	}
+}
+
+func (c *ContaCorrente) ObterSaldo() float64 {
+	return c.saldo
 }
--- src/ProjetoAluraBancoEmGo/main.go
+++ src/ProjetoAluraBancoEmGo/main.go
@@ -6,16 +6,8 @@ import (
 )
 
 func main() {
-	// Correção 1: Adicionado o prefixo "contas."
-	// Correção 2: Campos com letra maiúscula (Titular, Saldo)
-	contaDaSilvia := contas.ContaCorrente{Titular: "Silvia", Saldo: 300}
-	contaDoGustavo := contas.ContaCorrente{Titular: "Gustavo", Saldo: 500}
+	contaExemplo := contas.ContaCorrente{}
+	contaExemplo.Depositar(100)
 
-	// Lembre-se de usar um valor válido para a transferência!
-	// Ex: um valor positivo e que Gustavo tenha em conta.
-	status := contaDoGustavo.Transferir(150, &contaDaSilvia)
-
-	fmt.Println(status)
-	fmt.Println(contaDaSilvia)
-	fmt.Println(contaDoGustavo)
+	fmt.Println(contaExemplo.ObterSaldo())
 }
